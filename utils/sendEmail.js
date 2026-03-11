ba0e6609d2620d94ee4ed6d41fdd78c330edcd96
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-03-11 19:09:21 +0200

    last commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,27 @@
 const nodemailer = require("nodemailer");
 
 exports.sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false, // STARTTLS على port 587
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD, // App Password
+      },
+    });
 
-  const mailOpts = {
-    from: "petsify App <brahimbendada024@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    html: options.html,
-  };
+    const mailOpts = {
+      from: `"Petsify App" <${process.env.EMAIL_USER}>`,
+      to: options.email,
+      subject: options.subject,
+      html: options.html,
+    };
 
-  await transporter.sendMail(mailOpts);
+    await transporter.sendMail(mailOpts);
+    console.log("Email sent successfully to:", options.email);
+  } catch (error) {
+    console.log("Failed to send email:", error);
+  }
 };
