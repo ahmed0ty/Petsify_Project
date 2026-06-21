62d257d94e45185e13ac63eae874dddaa6eb6ec3
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-06-21 04:04:48 +0300

    fix: add delete product route

--- routes/product_route.js
+++ routes/product_route.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct , getPetProductsBySellerId, getAllProducts} = require("../controllers/product_controller");
+const { createProduct, getPetProductsBySellerId, getAllProducts, deleteProduct } = require("../controllers/product_controller");
 const { uploadImage } = require("../middlewares/imageMiddleware");
 const { createProductValidator } = require("../utils/validator/product_validator");
 
-
-
-router.route("/").get(getAllProducts)
+router.route("/").get(getAllProducts);
 router.route("/").post(
   uploadImage("product").fields([
     { name: "picture", maxCount: 1 },
@@ -15,5 +13,6 @@ router.route("/").post(
   createProduct
 );
 router.route("/seller/:sellerId").get(getPetProductsBySellerId);
+router.route("/:id").delete(deleteProduct);
 
 module.exports = router;
